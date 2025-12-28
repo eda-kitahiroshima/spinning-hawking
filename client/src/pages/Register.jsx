993b9caa27b82e6392fadb62841888e1fbc94ec2
eda-kitahiroshima <eda1130atsushi6053@gmail.com>
2025-12-28 10:15:25 +0900

    Fix duplicate import in Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API_BASE_URL from '../config';
-import API_BASE_URL from '../config';
 
 const Register = () => {
     const navigate = useNavigate();
