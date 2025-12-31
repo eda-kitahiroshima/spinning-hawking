1ae75627b82f74b08b03f6e39bc4f7fca9d0a822
eda-kitahiroshima <eda1130atsushi6053@gmail.com>
2026-01-01 01:11:42 +0900

    Fix: Use middleware for React app serving (Express 5)

--- server/index.js
+++ server/index.js
@@ -363,7 +363,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // All other routes return the React app (Express 5 compatible)
-  app.get('/(.*)', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
